a2d9eed66c6391d89cdedd7c593ee5b7dbc4c487
beefly-repo <hungry_foolish_ing@163.com>
2026-04-12 05:41:33 +0900

    control the font color,size and distance

--- interface.py
+++ interface.py
@@ -21,8 +21,25 @@ rgb_calculate_accuracy = 16 #可以更改
 #图片显示RGB和Con.的小数点位数，可以单独控制
 rgb_display_accuracy = 2 #可以更改
 con_display_accuracy = 2 #可以更改
-#选择相应的颜色通道
-color_channel = 'R' # 'R' 'G' 'B' 三选一
+#选择相应的颜色通道+++++++++++
+color_channel = 'G' # 'R' 'G' 'B' 三选一
+#控制Con R G B三行的显示顺序
+Order_Con_R_G_B = 'ConRGB' #'ConRGB,ConRBG,ConGRB,ConGBR,ConBRG,ConBGR' 多选一
+#控制No Con R G B五行显示的颜色,格式为(B,G,R)，数值从0-255； (0, 0, 0)为黑色 (255, 255, 255)为白色
+color_No = (255, 0, 255) #可以更改 
+color_Con = (255, 255, 0) #可以更改 
+color_R = (0, 0, 255) #可以更改
+color_G = (0, 255, 0) #可以更改
+color_B = (255, 0, 0) #可以更改
+#控制No. 与cuvette之间的间距
+distance_between_No_cuvette = 50 #可以更改
+#控制cuvette和Con之间的间距
+distance_between_cuvette_Con = 50 #可以更改
+#控制Con、R、G、B之间的间距
+distance_between_Con_R_G_B = 10 #可以更改
+#显示字体大小和类型
+output_font_size = 60 #可以更改
+output_font = "Arial.ttf" #可以更改，谨慎更改 # "Arial.ttf"
 #是否添加指示灯
 add_light = False
 
